7fc87aec5259b6039a8abba28a730a677b5e5110
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-17 12:13:42 +0900

    코드

--- Page/page.py
+++ Page/page.py
@@ -1,3 +1,26 @@
+import random
+import pandas as pd
 import streamlit as st
 
-st.write("Hello, World!")
+items = ['item1', 'item2', 'item3', 'item4']
+result = []
+
+def open_box():
+    if not items:
+        return None
+    item = random.choice(items)
+    items.remove(item)
+    return item
+
+while st.button('Open Box'):
+    item = open_box()
+    if item:
+        st.write(f"You got {item}!")
+        result.append(item)
+    else:
+        st.write("The box is empty!")
+        break
+
+df = pd.DataFrame(result, columns=['Items'])
+st.write("Result:")
+st.write(df)
