6223ac9481690984f590d96a357941b770ad9265
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-12-30 16:28:45 +0900

    문제 만들기

--- Page/page2.py
+++ Page/page2.py
@@ -1,22 +1,20 @@
 import streamlit as st
-# from streamlit_option_menu import option_menu
 
-st.write("안녕")
+st.title("퀴즈 만들어보기")
 password1 = "창설이벤트"
-answer1 = "아기자기"
+answer1 = "바나나"
  # 1번
-quiz1_password = st.text_input("1번 문제 오픈을 위한 비밀번호를 입력해주세요!",  key="quiz1_password")
+quiz1_password = st.text_input("퀴즈를 풀고싶다면, [도전]이라고 적어주세요",  key="quiz1_password")
 if quiz1_password == password1:
-    quiz1 = st.text_input("우리 길드의 이름은 뭘까?(⊙_⊙)？")
+    quiz1 = st.text_input("원숭이가 좋아하는 과일은？")
     if st.button("정답 확인", key="check_answer_button1"):
         if quiz1 == answer1:
             st.balloons()
             st.success("정답입니다!")
             st.write("우리 길드와 함께해줘서 고마워 ╰(*°▽°*)╯")
-            st.write("2번 문제 오픈을 위한 비밀번호는 '아깅이' 입니다")
         else:
             st.warning("다시 한 번 생각해봐!")
     if st.button("힌트 보기", key="check_hint_button1"):
-            st.write("이건 힌트를 줄 수가 없어! 잘 생각해 봐")
+            st.write("노랗고 길죽한 과일이야!")
 elif quiz1_password != "" and quiz1_password != password1:
     st.error("비밀번호가 틀렸어!")
