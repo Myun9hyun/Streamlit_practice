0c23a052452e65fefeb2b48ca1ce529c1fb296e9
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-12-30 16:36:42 +0900

    수정

--- Page/page2.py
+++ Page/page2.py
@@ -2,19 +2,19 @@ import streamlit as st
 
 st.title("퀴즈 만들어보기")
 password1 = "도전"
-answer1 = "바나나"
- # 1번
+answer1 = "python" or "파이썬"
+
 quiz1_password = st.text_input("퀴즈를 풀고싶다면, [도전]이라고 적어주세요",  key="quiz1_password")
 if quiz1_password == password1:
-    quiz1 = st.text_input("원숭이가 좋아하는 과일은？")
+    quiz1 = st.text_input("Streamlit을 구성하는 언어는 무엇일까요?")
     if st.button("정답 확인", key="check_answer_button1"):
         if quiz1 == answer1:
             st.balloons()
             st.success("정답입니다!")
             st.write("원숭이가 좋아하는 과일은 바나나야! ╰(*°▽°*)╯")
         else:
-            st.warning("다시 한 번 생각해봐!")
+            st.warning("정답이 아니야. 다시 생각해봐")
     if st.button("힌트 보기", key="check_hint_button1"):
-            st.write("노랗고 길죽한 과일이야!")
+            st.write("p로 시작하는 언어야")
 elif quiz1_password != "" and quiz1_password != password1:
     st.error("비밀번호가 틀렸어!")
